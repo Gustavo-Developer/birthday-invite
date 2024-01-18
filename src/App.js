b3d144d220edfabe7653dd502bc9bcdbf608e990
Gustavo <srto.mendes@gmail.com>
2024-01-18 17:52:08 -0300

    edit layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ReactAudioPlayer from "react-audio-player";
+
+import parabensvideo from "./assets/parabens.mp4";
+import parabensaudio from "./assets/parabens-audio.mp3";
+import meme from "./assets/meme.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactAudioPlayer src={parabensaudio} autoPlay={true} />
+      <div className="video">
+        <div>
+          <video
+            src={parabensvideo}
+            autoPlay="true"
+            width="100%"
+            height="100%"
+            muted
+            style={{
+              position: "absolute",
+            }}
+          />
+
+          <div
+            className="text"
+            style={{
+              position: "relative",
+            }}
+          >
+            <div>
+              <img
+                src={meme}
+                alt="Meme"
+                style={{ width: "100px", height: "auto" }}
+              />
+
+              <p>REPI BARDEI TIU IU</p>
+
+              <div>
+                <p>ANANDA</p>
+                <p style={{ color: "#C0392B" }}>21 ANOS</p>
+              </div>
+
+              <p>SABADO, 3 DE FEVEREIRO AS 16H</p>
+              <p>AQUI EM CASA MESMO</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
