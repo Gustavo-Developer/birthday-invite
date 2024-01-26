daf4c5f32e90084bc73896533d4ae9f46620c991
Gustavo Mendes <71361227+Gustavo-Developer@users.noreply.github.com>
2024-01-26 13:47:19 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import meme from "./assets/meme.png";
 function App() {
   return (
     <div className="App">
-      <div className="gradient-border content" id="box">
+    <div style={{display: "hidden"}}>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/EEhGLRl--LE?si=Qpu5SUTrRsdHgYiz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen />
+    </div> 
+    <div className="gradient-border content" id="box">
         <img
           className="image"
           src={meme}
